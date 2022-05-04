6e2e79a782f2c68d9a78e9ee029ab8226fa93e56
0chaowenguo <104413753+0chaowenguo@users.noreply.github.com>
2022-05-04 05:45:39 -0500

    Update browserstack.js

--- browserstack.js
+++ browserstack.js
@@ -11,11 +11,17 @@ const caps =
     'browserstack.idleTimeout':'300',
     'client.playwrightVersion':child_process.spawnSync('npx', ['playwright', '--version']).stdout.toString().trim().split(' ').at(-1) // Playwright version being used on your local project needs to be passed in this capability for BrowserStack to be able to map request and responses correctly
 }
-const browser = await firefox.connect({wsEndpoint:`wss://cdp.browserstack.com/playwright?caps=${globalThis.encodeURIComponent(globalThis.JSON.stringify(caps))}`})
-const context = await browser.newContext()
-const alexamaster = await context.newPage()
-const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.goto('https://www.alexamaster.net/ads/autosurf/157701')])
-await popup.bringToFront()
-const blank = await browser.newPage()
-context.on('page', async _ => await _.close())
-globalThis.setInterval(async _ => await blank.reload(), 1000 * 60)
+
+async function session()
+{
+    const browser = await firefox.connect({wsEndpoint:`wss://cdp.browserstack.com/playwright?caps=${globalThis.encodeURIComponent(globalThis.JSON.stringify(caps))}`})
+    const context = await browser.newContext()
+    const alexamaster = await context.newPage()
+    const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.goto('https://www.alexamaster.net/ads/autosurf/157701')])
+    await popup.bringToFront()
+    const blank = await browser.newPage()
+    context.on('page', async _ => await _.close())
+    globalThis.setInterval(async _ => await blank.reload(), 1000 * 60)
+}
+
+await globalThis.Promise.all(globalThis.Array.from({length:5}, (_, index) => session()))
