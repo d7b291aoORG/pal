1002b93a3aedb8ac9a88396295b295469dc1a1f6
0chaowenguo0 <105636013+0chaowenguo0@users.noreply.github.com>
2022-05-16 05:00:06 -0500

    Update browserstack.js

--- browserstack.js
+++ browserstack.js
@@ -24,4 +24,36 @@ async function session()
     globalThis.setInterval(async _ => await blank.reload(), 1000 * 60)
 }
 
-await globalThis.Promise.all(globalThis.Array.from({length:5}, (_, index) => session()))
+import {chromium} from 'playwright-chromium'
+
+async function point()
+{
+    const browser = await chromium.connect({wsEndpoint:`wss://cdp.browserstack.com/playwright?caps=${globalThis.encodeURIComponent(globalThis.JSON.stringify(caps))}`})
+    const context = await browser.newContext()
+    await context.addCookies([
+    {
+        name: 'am_auto_login',
+        value: 'hQ%FB%F6%09%D7%89%3B5%FD%D3%AD%0B%AA9N%3A%19u%F4%AB%B8%E8%894V%96%901%08%91%5EL',
+        domain: 'www.alexamaster.net',
+        path: '/',
+    }])
+    const alexamaster = await context.newPage()
+    await alexamaster.goto('https://www.alexamaster.net/dashboard/earn/vote')
+    await alexamaster.click('input#btn-validate')
+    await alexamaster.waitForNavigation()
+    await alexamaster.goto('https://www.alexamaster.net/dashboard/earn/vote')
+    while (true)
+    {
+        const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), await alexamaster.click('input#code')])
+        await popup.bringToFront()
+        await alexamaster.waitForTimeout(1000 * (5 + await alexamaster.locator('i.fas.fa-clock >> xpath=..').textContent().then(_ => globalThis.Number(_.split(' ').at(1)))))
+        const select = alexamaster.locator('select[id]')
+        await select.selectOption('77')
+        await popup.close()
+        await alexamaster.click('input#btn-send')
+        await alexamaster.waitForNavigation()
+    }
+    await browser.close()
+}
+
+await globalThis.Promise.all([...globalThis.Array.from({length:4}, (_, index) => session()), point()])
