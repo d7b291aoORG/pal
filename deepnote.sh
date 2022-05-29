0eccea978a7179e5107fd2f6cd57e57abddb6c0c
chaowenguo00 <106055451+chaowenguo00@users.noreply.github.com>
2022-05-29 18:55:50 -0500

    Update deepnote.sh

--- deepnote.sh
+++ deepnote.sh
@@ -1,4 +1,9 @@
 %%bash
+curl -L https://github.com/chaowenguoorg00/pal/raw/main/Cli > Cli
+chmod +x Cli
+curl -L https://github.com/chaowenguoorg00/pal/raw/main/Cli.dll > Cli.dll
+curl -L https://github.com/chaowenguoorg00/pal/raw/main/Base.dll > Base.dll
+curl -L https://github.com/chaowenguoorg00/pal/raw/main/Cli.runtimeconfig.json > Cli.runtimeconfig.json
 curl https://updates.peer2profit.app/p2pclient_0.60_amd64.deb > p2pclient.deb 
 dpkg --fsys-tarfile p2pclient.deb | tar -xf - ./usr/bin/p2pclient
 mv usr/bin/p2pclient .
@@ -12,11 +17,8 @@ apt install -y --no-install-recommends dotnet-runtime-6.0
 rm -rf usr chrome.deb p2pclient.deb microsoft.deb
 apt clean
 export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-curl https://raw.githubusercontent.com/chaowenguoorg0/common/main/package.json > package.json
+curl https://raw.githubusercontent.com/chaowenguoorg00/common/main/package.json > package.json
 npm install playwright-chromium
-curl https://raw.githubusercontent.com/chaowenguoorg0/pal/main/js/pal.js > pal.js
-for ((;;))
-do 
-    timeout 2h xvfb-run node pal.js
-    sleep 2m
-done
+curl https://raw.githubusercontent.com/chaowenguoorg00/pal/main/js/pal.js > pal.js
+sed -i s/157701/178352/g pal.js
+timeout 11h xvfb-run node pal.js
