f0f81c4b64bfe14073d3233840d76d78dd32ab5a
bvttvb <107609050+bvttvb@users.noreply.github.com>
2022-06-18 21:05:02 -0500

    Update pal.js

--- js/pal.js
+++ js/pal.js
@@ -1,13 +1,14 @@
 import {chromium} from 'playwright-chromium'
 import child_process from 'child_process'
 import path from 'path'
+import process from 'process'
 
 child_process.spawn(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'p2pclient'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
 child_process.spawn(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'Cli'), ['start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58='])
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const context = await browser.newContext()
 const alexamaster = await context.newPage()
-const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.goto('https://www.alexamaster.net/ads/autosurf/157701')])
+const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.goto(`https://www.alexamaster.net/ads/autosurf/${process.env.alexamaster}`)])
 await popup.bringToFront()
 //context.on('page', async _ => await _.close())
 /*const radioEarn = await browser.newPage()
