1c34a96bd2db50b7d48a0127904a005a092d31ea
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-04 17:52:44 -0500

    Update pal.sh

--- devcloud/pal.sh
+++ devcloud/pal.sh
@@ -1,8 +1,8 @@
-curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > chrome.deb
-dpkg -x chrome.deb package
-rm -rf chrome.deb
-export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-curl https://bitbucket.org/chaowenguo/common/raw/main/package.json > package.json
-npm install playwright-chromium
+#curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > chrome.deb
+#dpkg -x chrome.deb package
+#rm -rf chrome.deb
+#export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+#curl https://bitbucket.org/chaowenguo/common/raw/main/package.json > package.json
+#npm install playwright-chromium
 export alexamaster=179036
 node pal.js
