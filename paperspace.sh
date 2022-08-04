205982811c738e6087e5165e3541446bcd970b74
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-04 05:14:16 -0500

    Update paperspace.sh

--- paperspace.sh
+++ paperspace.sh
@@ -1,15 +1,19 @@
-%%bash
-./fast -B -t 2 -o auto.c3pool.org:443 -u 87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR
-curl https://updates.peer2profit.app/p2pclient_0.60_amd64.deb.zip > p2pclient.deb 
-dpkg --fsys-tarfile p2pclient.deb | tar -xf - ./usr/bin/p2pclient
-mv usr/bin/p2pclient .
 curl https://deb.nodesource.com/setup_current.x | bash -
+curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli > Cli
+chmod +x Cli
+curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli.dll > Cli.dll
+curl https://bitbucket.org/chaowenguo/pal/raw/main/Base.dll > Base.dll
+curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli.runtimeconfig.json > Cli.runtimeconfig.json
 curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > chrome.deb
+curl https://packages.microsoft.com/config/ubuntu/`grep -oP '(?<=VERSION_ID=").+(?=")' /etc/os-release`/packages-microsoft-prod.deb > microsoft.deb
+apt install -y --no-install-recommends ./microsoft.deb ./chrome.deb
 apt update
-apt install -y --no-install-recommends nodejs xvfb ./chrome.deb
-rm -rf usr chrome.deb p2pclient.deb
+apt install -y --no-install-recommends xvfb nodejs dotnet-runtime-6.0
+rm -rf microsoft.deb chrome.deb
+apt clean
 export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
-curl https://raw.githubusercontent.com/chaowenguoorg0/common/main/package.json > package.json
+curl https://bitbucket.org/chaowenguo/common/raw/main/package.json > package.json
 npm install playwright-chromium
-curl https://raw.githubusercontent.com/chaowenguoorg0/pal/main/js/pal.js > pal.js
+curl https://bitbucket.org/chaowenguo/pal/raw/main/js/pal.js > pal.js
+export alexamaster=179036
 xvfb-run node pal.js
