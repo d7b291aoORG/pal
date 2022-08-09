677ea6f24cfb04e0df5624a6158e723ae21de666
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-09 11:54:11 -0500

    Update paperspace.js

--- paperspace.js
+++ paperspace.js
@@ -4,7 +4,7 @@ import process from 'process'
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
 const paperspace = await browser.newPage({recordVideo:{dir:'videos'}, viewport:null})
 await paperspace.goto(`https://console.paperspace.com/login`)
-await paperspace.fill('input[type="email"]', 'chaowen.guo0@gmail.com')
+await paperspace.fill('input[type="email"]', 'chaowen.guo1@gmail.com')
 await paperspace.fill('input[type="password"]', process.argv.at(2))
 await paperspace.click('button.c-jpjkst.c-jpjkst-kkVNWy-variant-cta')
 await paperspace.waitForNavigation()
