3bad309c59c174e44fa2b2dae24a2e5d6ebd57d3
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-11 21:21:42 -0500

    Update 12h.js

--- 12h.js
+++ 12h.js
@@ -2,7 +2,7 @@ import {chromium} from 'playwright-chromium'
 import process from 'process'
 
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
-/*const sagemaker = await browser.newPage({recordVideo:{dir:'videos'}, viewport:null})
+const sagemaker = await browser.newPage({recordVideo:{dir:'videos'}, viewport:null})
 await sagemaker.goto('https://studiolab.sagemaker.aws/login')
 await sagemaker.fill('input[name="username"]', 'chaowen.guo1@gmail.com')
 await sagemaker.fill('input[name="password"]', process.argv.at(2))
@@ -12,7 +12,7 @@ const [popup] = await globalThis.Promise.all([sagemaker.waitForEvent('popup'), s
 await popup.dblclick('li[title^="Name: pal.ipynb"]')
 await popup.click('button[data-command="runmenu:restart-and-run-all"]')
 await popup.click('button.jp-mod-accept')
-await popup.waitForTimeout(10 * 1000)*/
+await popup.waitForTimeout(10 * 1000)
 
 const devcloud = await browser.newPage({recordVideo:{dir:'videos'}, viewport:null})
 await devcloud.goto('https://www.intel.com/content/www/us/en/my-intel/devcloud-sign-in.html')
