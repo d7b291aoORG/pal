666efa72bf7aab4c780c76ef3fdc2a322af0205e
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-16 00:53:19 -0500

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -6,7 +6,7 @@ chmod +x Cli
 curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli.dll > Cli.dll
 curl https://bitbucket.org/chaowenguo/pal/raw/main/Base.dll > Base.dll
 curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli.runtimeconfig.json > Cli.runtimeconfig.json
-curl https://packages.microsoft.com/config/ubuntu/`grep -oP '(?<=VERSION_ID=").+(?=")' /etc/os-release`/packages-microsoft-prod.deb > microsoft.deb
+curl https://packages.microsoft.com/config/ubuntu/$(grep -oP '(?<=VERSION_ID=").+(?=")' /etc/os-release)/packages-microsoft-prod.deb > microsoft.deb
 apt install -y --no-install-recommends ./microsoft.deb
 apt update
 apt install -y --no-install-recommends chromium-browser xvfb nodejs dotnet-runtime-6.0
