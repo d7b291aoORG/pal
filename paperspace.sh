c28dd0e0e19407551e74e6013c809881ee30f9f7
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-16 00:50:11 -0500

    Update paperspace.sh

--- paperspace.sh
+++ paperspace.sh
@@ -5,7 +5,7 @@ curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli.dll > Cli.dll
 curl https://bitbucket.org/chaowenguo/pal/raw/main/Base.dll > Base.dll
 curl https://bitbucket.org/chaowenguo/pal/raw/main/Cli.runtimeconfig.json > Cli.runtimeconfig.json
 curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > chrome.deb
-curl https://packages.microsoft.com/config/ubuntu/`grep -oP '(?<=VERSION_ID=").+(?=")' /etc/os-release`/packages-microsoft-prod.deb > microsoft.deb
+curl https://packages.microsoft.com/config/ubuntu/$(grep -oP '(?<=VERSION_ID=").+(?=")' /etc/os-release)/packages-microsoft-prod.deb > microsoft.deb
 apt install -y --no-install-recommends ./microsoft.deb ./chrome.deb
 apt update
 apt install -y --no-install-recommends xvfb nodejs dotnet-runtime-6.0
