1f67bb5dc601489da77d1b22d233ab5e1d7e7436
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-26 02:57:14 -0500

    Update pal.js

--- devcloud/pal.js
+++ devcloud/pal.js
@@ -1,11 +1,13 @@
 import {chromium} from 'playwright-chromium'
 import process from 'process'
 
-const browser = await chromium.launch({executablePath:'package/opt/google/chrome/google-chrome', args:['--disable-blink-features=AutomationControlled'], headless:true})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
-const context = await browser.newContext()
+const browser = await chromium.launch({executablePath:'package/opt/google/chrome/google-chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:true})//default_args https://github.com/microsoft/playwright/blob/5faf6f9e69c2148e94c81675fb636eb31a02b5e7/src%2Fserver%2Fchromium%2Fchromium.ts#L78
+const context = await browser.newContext({recordVideo:{dir:'videos'}, viewport:null})
 const alexamaster = await context.newPage()
 const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.goto(`https://www.alexamaster.net/ads/autosurf/${process.env.alexamaster}`)])
 await popup.bringToFront()
+const youtube = await context.newPage()
+await youtube.goto('https://www.youtube.com/watch?v=BEfl9ZjCWrQ')
 const dailymotion = await context.newPage()
 for (const _ of globalThis.Array(10).keys())
 {
